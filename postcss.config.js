be5c75c94eeb294651c1f6f632c821d8a7a7dc04
Coding in Public <86967271+coding-in-public@users.noreply.github.com>
2022-03-21 12:15:25 -0600

    Video code example

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+const postcssJitProps = require('postcss-jit-props');
+const OpenProps = require('open-props');
+
+module.exports = {
+  plugins: [
+    postcssJitProps(OpenProps),
+  ]
+}
